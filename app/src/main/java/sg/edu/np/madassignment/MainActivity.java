5c547436fef76d051acbadc697baa20b22977229
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-12 17:51:32 +0800

    Added onboarding page for app

--- app/src/main/java/sg/edu/np/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/MainActivity.java
@@ -1,10 +1,16 @@
 package sg.edu.np.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager_slider;
+    ArrayList<OnBoardPageItems> viewPagerItemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //test
+        viewPager_slider = findViewById(R.id.viewpager);
+        int[] images = {R.drawable.chef,R.drawable.community,R.drawable.share};
+        String[] heading = {"Cooking experience like a chef ","Learn how the community cooks","Share your recipes with others"};
+        String[] desc = {"Let's make a delicious dish with the best recipes for you and your family",
+                "Exploring your community taste buds and what food means to them",
+                "Sharing recipes and sharing the joy of food with others"};
+
+        viewPagerItemArrayList = new ArrayList<>();
+
+        for (int i =0; i< images.length ; i++){
+            OnBoardPageItems onBoardItem = new OnBoardPageItems(images[i],heading[i],desc[i]);
+            viewPagerItemArrayList.add(onBoardItem);
+        }
+
+        OnBoardPageAdapter OBAdapter = new OnBoardPageAdapter(viewPagerItemArrayList);
+        viewPager_slider.setAdapter(OBAdapter);
     }
 }
