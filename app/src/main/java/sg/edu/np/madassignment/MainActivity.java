2a53f448bc9184367ef583715d875c9a806cf4a0
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-14 12:52:43 +0800

    updated On Board page Main Activity

--- app/src/main/java/sg/edu/np/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/MainActivity.java
@@ -3,7 +3,11 @@ package sg.edu.np.madassignment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -34,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
 
         OnBoardPageAdapter OBAdapter = new OnBoardPageAdapter(viewPagerItemArrayList);
         viewPager_slider.setAdapter(OBAdapter);
+
+        Button get_started = findViewById(R.id.getStarted);
+        get_started.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            }
+        });
+
     }
 }
