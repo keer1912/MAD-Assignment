6269ca026b2564494b6d8ed61aaf587018ae9ba1
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-14 20:26:13 +0800

    Added dots for onboarding page

--- app/src/main/java/sg/edu/np/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView[] dots;
+    LinearLayout layout;
     ViewPager2 viewPager_slider;
     ArrayList<OnBoardPageItems> viewPagerItemArrayList;
 
@@ -21,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //test
         viewPager_slider = findViewById(R.id.viewpager);
+        layout = findViewById(R.id.dots_container);
+
+        dots = new TextView[3];
+
         int[] images = {R.drawable.chef,R.drawable.community,R.drawable.share};
         String[] heading = {"Cooking experience like a chef ","Learn how the community cooks","Share your recipes with others"};
         String[] desc = {"Let's make a delicious dish with the best recipes for you and your family",
@@ -47,5 +56,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        setIndicators();
+        viewPager_slider.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                selectedDots(position);
+                super.onPageSelected(position);
+            }
+        });
     }
+
+    private void selectedDots(int position) {
+        for (int i = 0; i < dots.length; i++) {
+            if (i == position) {
+                dots[i].setTextColor(getResources().getColor(R.color.purple));
+            } else {
+                dots[i].setTextColor(getResources().getColor(R.color.light_grey));
+            }
+        }
+    }
+
+    private void setIndicators() {
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#9679;"));
+            dots[i].setTextSize(24);
+            layout.addView(dots[i]);
+        }
+
+    }
+
 }
