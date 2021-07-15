3c0d2ac71c19cd79eddf04080e60c33a8034cb1e
Kai Ze Aaron Sng <s10194187@connect.np.edu.sg>
2021-07-15 13:10:24 +0800

    added profile layout

--- app/src/main/java/sg/edu/np/madassignment/ProfileActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/ProfileActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.madassignment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,10 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
     TextView userName;
+    ImageView profilePic;
+    TextView email;
     private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -20,14 +24,22 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //get user
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
         if(user!= null){
             userName = findViewById(R.id.userName);
             userName.setText(user.getDisplayName());
+
+            profilePic = findViewById(R.id.profilePic);
+//            profilePic.setImageResource(user.get);
+
+            email = findViewById(R.id.email);
+            email.setText(user.getEmail());
         }
 
+        //sign out
         findViewById(R.id.logOut).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
