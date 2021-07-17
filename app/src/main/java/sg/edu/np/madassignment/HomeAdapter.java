7d73681829c0103e5505205e3ae7c8d8b77f12d3
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-17 15:50:45 +0800

    Attempt Home Activity RV from firebase

---
+++ app/src/main/java/sg/edu/np/madassignment/HomeAdapter.java
@@ -0,0 +1,44 @@
+package sg.edu.np.madassignment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeVH>{
+    ArrayList<Recipe> recipeArrayList;
+    Context mcontext;
+
+    public HomeAdapter(Context c,ArrayList<Recipe> input) {
+        mcontext = c;
+        recipeArrayList = input;
+    }
+
+    @NonNull
+    @Override
+    public HomeVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.activity_home_rv_item,
+                parent,
+                false);
+        return new HomeVH(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeVH holder, int position) {
+        Recipe recipe = recipeArrayList.get(position);
+        holder.recipeName.setText(recipe.getName());
+        holder.recipeTime.setText(recipe.getTime());
+        //holder.recipeImg.s(recipe.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeArrayList.size();
+    }
+}
