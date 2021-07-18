d705362b2aefb3494c17ab51b2545d2fafc51e0f
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-18 23:54:27 +0800

    added on click listener for RV item and intent to send details

--- app/src/main/java/sg/edu/np/madassignment/HomeAdapter.java
+++ app/src/main/java/sg/edu/np/madassignment/HomeAdapter.java
@@ -1,12 +1,15 @@
 package sg.edu.np.madassignment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -34,7 +37,22 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeVH>{
         Recipe recipe = recipeArrayList.get(position);
         holder.recipeName.setText(recipe.getName());
         holder.recipeTime.setText(String.valueOf(recipe.getTime()));
-        //holder.recipeImg.s(recipe.getImage());
+        //Picasso.with(mcontext).load(recipe.getImg()).into(holder.recipeImg);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(),RecipeDetail.class);
+                intent.putExtra("name",recipe.getName());
+                intent.putExtra("category",recipe.getCategory());
+                intent.putExtra("time",recipe.getTime());
+                intent.putExtra("difficulty",recipe.getDifficulty());
+                intent.putExtra("likes",recipe.getLikes());
+                intent.putExtra("owner",recipe.getOwner());
+
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
