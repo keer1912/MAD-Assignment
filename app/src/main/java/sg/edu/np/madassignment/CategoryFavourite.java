04ce15b5ed8db54bc7967cff1fe57792f620ebb9
Kai Ze Aaron Sng <s10194187@connect.np.edu.sg>
2021-07-23 14:18:30 +0800

    added listview for profile page

---
+++ app/src/main/java/sg/edu/np/madassignment/CategoryFavourite.java
@@ -0,0 +1,32 @@
+package sg.edu.np.madassignment;
+
+public class CategoryFavourite {
+
+    private String category;
+    private int numberOfFavourite;
+    private int categoryDrawable;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getNumberOfFavourite() {
+        return numberOfFavourite;
+    }
+
+    public void setNumberOfFavourite(int numberOfFavourite) {
+        this.numberOfFavourite = numberOfFavourite;
+    }
+
+    public int getCategoryDrawable() {
+        return categoryDrawable;
+    }
+
+    public void setCategoryDrawable(int categoryDrawable) {
+        this.categoryDrawable = categoryDrawable;
+    }
+}
--- app/src/main/java/sg/edu/np/madassignment/LoginActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/LoginActivity.java
@@ -71,6 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
+                .requestProfile()
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
