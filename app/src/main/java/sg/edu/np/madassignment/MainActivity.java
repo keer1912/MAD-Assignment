bbad3b588701f7d0dcf76edbb103bda603e2d60b
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-07-23 15:09:39 +0800

    minor change

--- app/src/main/java/sg/edu/np/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/MainActivity.java
@@ -23,10 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // THIS TIMER CODE WILL ONLY BE EXECUTED WHEN USER IS LOGGED IN.
-        // if user is logged in the flow is SplashActivity ----> LoginActivity (happen in background) ----> HomeActivity
-        //else if user is not then the flow is MainActivity ---->LoginActivity (shown) ---->HomeActivity
-
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
